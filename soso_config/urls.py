58c0b3d012a96b1b2421125ae2e2f1be3aaa081d
jonahtusubira261-jpg <jonahtusubira261@gmail.com>
2026-02-27 13:04:11 +0300

    Update urls.py

--- soso_config/urls.py
+++ soso_config/urls.py
@@ -3,6 +3,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from core.views import health_check
+
 
 urlpatterns = [
     # Admin Panel
@@ -14,6 +16,7 @@ urlpatterns = [
     # Built-in Auth Handlers
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
+    path('health/', health_check, name='health_check'),
 ]
 
 # Essential for showing product images during development
